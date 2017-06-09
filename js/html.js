ab9730066b6718c6ad875eb544602de1e85de9c9
Matt Currier <fingerskier@gmail.com>
2017-06-09 14:33:27 -0500

    Update html.js

--- js/html.js
+++ js/html.js
@@ -1,40 +1,80 @@
-function a(href, text){
-    var href = href || ''
-    var text = text || ''
-
-    return `<a href="${href}">${text}<\a>`
-}
-
 function parseChildren(){
     var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
     var result = ''
 
     for (var X of args) {
-        if ($.isFunction(X)){
-            result += X()
-        } else if ($.isArray(X)) {
-            for(var Y of X){
-                result += parseChildren(Y)
+        if (X){
+            if ($.isFunction(X)){
+                result += X()
+            } else if ($.isArray(X) || $.isPlainObject(X)) {
+                for(var Y of X){
+                    result += parseChildren(Y)
+                }
+            } else if (X.toString) {
+                result += X.toString()
+            } else {
+                result += X
             }
-        } else if ($.isPlainObject(X)) {
-            let O = (X.length === 1 ? [O[0]] : Array.apply(null, X))
-            for(var Y of X){
-                result += parseChildren(Y)
-            }            
-        } else if (X.toString) {
-            result += X.toString()
-        } else {
-            result += X
         }
     }
 
     return result
 }
 
+function tag(name, attr, ...content){
+    var attrs = ''
+    var guts = ''
+
+    if ($.isPlainObject(attr)){
+        for (var X in attr){
+            attrs += `${X}="${attr[X]}"`
+        }
+    } else {
+        guts = parseChildren(attr)
+    }
+
+    return `<${name} ${attrs}>${parseChildren(guts)}${parseChildren(content)}</${name}>`
+}
+
+function a(href, ...content){
+    var href = href || ''
+
+    return tag('a', {href:href}, content)
+}
+
+function div(){
+console.log(arguments)
+    return tag('div', arguments)
+}
+
+function footr(){
+    return `<footer>${parseChildren(arguments)}</footer>`
+}
+
+function h1(){
+    return `<h1>${parseChildren(arguments)}</h1>`
+}
+
+function header(){
+    return `<header>${parseChildren(arguments)}</header>`
+}
+
 function hr(){
     return '<hr/>'
 }
 
+function img(src,alt){
+    return `<img src="${src}" alt="${alt}">`
+}
+
+function li(){
+    return `<li>${parseChildren(arguments)}</li>`
+}
+
 function nav(){
     return `<nav>${parseChildren(arguments)}</nav>`
+}
+
+function ul(){
+    return `<ul>${parseChildren(arguments)}</ul>`
 }
