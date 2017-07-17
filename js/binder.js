58a5e492c09724ba7c59824cb7bb121022455b97
Matt Currier <m.currier@premiercoil.com>
2017-07-17 15:03:47 -0500

    update

--- js/binder.js
+++ js/binder.js
@@ -1,24 +1,12 @@
-var binder_proxy = {
-    get(target, name) {
-        return function(strings, ...values) {
-            result = ''
+function binder(data){
+    return function(strings, ...values) {
+        result = ''
 
-            for (I of strings) {
-                result += strings[I]
-                result += this.data[values[I]]
-            }
-
-            return result
+        for (I of strings) {
+            result += strings[I]
+            result += data[values[I]]
         }
-    }
-    ,
-    set(target, prop, value, receiver) {
-        target[prop] = value
 
-        this.render()
+        return result
     }
 }
-
-var binder = new Proxy({
-    data: {}
-}, html_proxy);
